ec9d113ede6077e07f4e8413e109941b3731454a
Ryan Myers <ryan.p.myers@gmail.com>
2015-06-03 20:44:58 -0400

    Added an autoincrementing ID column to the DB.

--- create-db.py
+++ create-db.py
@@ -6,7 +6,7 @@ sqlConn = sqlite3.connect('w8.db')
 
 def main():
     cur = sqlConn.cursor()
-    cur.execute("CREATE TABLE w8upd8 IF NOT EXISTS (timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, weight REAL)")
+    cur.execute("CREATE TABLE w8upd8 IF NOT EXISTS (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, weight REAL)")
     sqlConn.commit()
     cur.close()
     sqlConn.close()
