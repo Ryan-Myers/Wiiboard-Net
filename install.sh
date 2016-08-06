8d21427c2923a5a39775f782a5394539961d2626
Ryan Myers <ryan.p.myers@gmail.com>
2016-08-06 15:42:06 +0000

    Fixed the service, andthe install script

--- install.sh
+++ install.sh
@@ -3,10 +3,16 @@
 rm -v /usr/sbin/wiiboardlistend
 rm -v /usr/sbin/wiiboardnet
 rm -v /usr/sbin/wiiboardnetd
-rm -v /etc/init.d/wiiboardnet-init
+rm -v /usr/sbin/wiiboardnet-init
+cp -v ./wiiboardnet.service /etc/systemd/system/wiiboardnet.service
+rm -v /usr/local/lib/python2.7/dist-packages/bluezutils.py
+rm -v /usr/local/lib/python2.7/dist-packages/bluezutils.pyc
 
 cp -v ./wiiboardlistend /usr/sbin/wiiboardlistend
 cp -v ./wiiboardnet /usr/sbin/wiiboardnet
 cp -v ./wiiboardnetd /usr/sbin/wiiboardnetd
-cp -v ./wiiboardnet-init /etc/init.d/wiiboardnet-init
+cp -v ./wiiboardnet-init /usr/sbin/wiiboardnet-init
+cp -v ./bluezutils.py /usr/local/lib/python2.7/dist-packages/
+cp -v ./wiiboardnet.service /etc/systemd/system/wiiboardnet.service
 
+systemctl enable wiiboardnet.service
